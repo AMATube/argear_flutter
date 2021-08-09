86a24e96c2d53023f81c7bb5bdf299db41f746b8
abc123931 <abc123931@gmail.com>
2021-08-09 10:23:03 +0900

    ✨ Add example.

---
+++ example/lib/constants.dart
@@ -0,0 +1,5 @@
+const defaultFilterItemId = String.fromEnvironment('DEFAULT_FILTER_ITEM_ID');
+const apiHost = String.fromEnvironment('API_HOST');
+const apiKey = String.fromEnvironment('API_KEY');
+const apiSecretKey = String.fromEnvironment('API_SECRET_KEY');
+const apiAuthKey = String.fromEnvironment('API_AUTH_KEY');
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,59 +1,142 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
+import 'dart:io';
 
-import 'package:flutter/services.dart';
+import 'package:argear_flutter_example/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:argear_flutter/argear_flutter.dart';
+import 'package:video_player/video_player.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await ArgearFlutter.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
+class MyApp extends HookWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
+      home: Home(),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class Home extends HookWidget {
+  Home({Key? key}) : super(key: key);
+
+  ARGearController? argearController;
+
+  @override
+  Widget build(BuildContext context) {
+    useEffect(() {
+      return () => argearController?.destroySession();
+    }, []);
+    return Scaffold(
+      body: Stack(children: [
+        ARGearPreview(
+          defaultFilterItemId: defaultFilterItemId,
+          apiHost: apiHost,
+          apiKey: apiKey,
+          apiSecretKey: apiSecretKey,
+          apiAuthKey: apiAuthKey,
+          onVideoRecorded: (path) {
+            Navigator.of(context).push(
+              MaterialPageRoute<PreviewPage>(
+                builder: (BuildContext context) {
+                  return PreviewPage(videoPath: path);
+                },
+              ),
+            );
+          },
+          argearCallback: (c) async {
+            argearController = c;
+          },
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+        Column(
+          children: [
+            TextButton(
+              onPressed: () {
+                argearController?.clearFilter();
+              },
+              child: const Text('フィルター削除'),
+            ),
+            TextButton(
+              onPressed: () {
+                argearController?.addFilter();
+              },
+              child: const Text('フィルター追加'),
+            ),
+            TextButton(
+              onPressed: () {
+                argearController?.startVideoRecording();
+              },
+              child: const Text('録画開始'),
+            ),
+            TextButton(
+              onPressed: () async {
+                await argearController?.stopVideoRecording();
+              },
+              child: const Text('録画終了'),
+            ),
+          ],
+        )
+      ]),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class PreviewPage extends HookWidget {
+  PreviewPage({Key? key, required this.videoPath}) : super(key: key);
+
+  final String videoPath;
+  VideoPlayerController? controller;
+
+  @override
+  Widget build(BuildContext context) {
+    final videoLoadingState = useState(true);
+
+    final initVideo = useCallback(() async {
+      try {
+        controller = VideoPlayerController.file(File(videoPath));
+        await controller?.initialize();
+      } finally {
+        videoLoadingState.value = false;
+      }
+    }, []);
+
+    useEffect(() {
+      initVideo();
+      return () => controller?.dispose();
+    }, []);
+
+    return Scaffold(
+      body: videoLoadingState.value
+          ? const Text('...loading')
+          : controller == null
+              ? const Text('ビデオが再生できません')
+              : VideoPlayerWidget(controller: controller!),
+    );
+  }
+}
+
+class VideoPlayerWidget extends HookWidget {
+  const VideoPlayerWidget({Key? key, required this.controller})
+      : super(key: key);
+
+  final VideoPlayerController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    useEffect(() {
+      controller.setLooping(true);
+      controller.play();
+    }, []);
+
+    return SafeArea(
+      child: Center(
+        child: VideoPlayer(controller),
       ),
     );
   }
